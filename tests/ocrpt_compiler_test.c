ce0189f5838182c21116cbc14fe95f9bbfadb942
Zoltán Böszörményi <zboszor@gmail.com>
2021-09-05 16:24:18 +0200

    Add a minimal test case to exercise #include <opencreport.h>
    Signed-off-by: Zoltán Böszörményi <zboszor@gmail.com>

---
+++ tests/ocrpt_compiler_test.c
@@ -0,0 +1,11 @@
+/*
+ * OpenCReports test
+ * Copyright (C) 2021-2021 Zoltán Böszörményi <zboszor@gmail.com>
+ * See COPYING.LGPLv3 in the toplevel directory.
+ */
+
+#include <opencreport.h>
+
+int main(void) {
+	return 0;
+}
