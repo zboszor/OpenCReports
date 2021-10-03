1b105f58a3f3ab393442f7b7a889b7ed889f7f28
Zoltán Böszörményi <zboszor@gmail.com>
2021-10-03 20:48:49 +0200

    Add forgotten variables.h header
    Signed-off-by: Zoltán Böszörményi <zboszor@gmail.com>

---
+++ libsrc/variables.h
@@ -0,0 +1,13 @@
+/*
+ * Variable utilities
+ * Copyright (C) 2019-2021 Zoltán Böszörményi <zboszor@gmail.com>
+ * See COPYING.LGPLv3 in the toplevel directory.
+ */
+#ifndef _VARIABLES_H_
+#define _VARIABLES_H_
+
+#include <opencreport.h>
+
+void ocrpt_variable_reset(opencreport *o, ocrpt_var *v);
+
+#endif
