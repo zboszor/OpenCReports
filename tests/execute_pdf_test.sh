eca231f1a681bcc06a8b490f2711e24ca2efb913
Zoltán Böszörményi <zboszor@gmail.com>
2022-07-19 10:16:39 +0200

    Use smarter unit test checks for PDF output
    All PDF output tests are inherently unstable because Cairo:
    * sets the /Producer PDF property with its own name and version
    * different Cairo versions support different PDF versions
    
    So they need to be treated differently: the new execute_pdf_test.sh
    unit test script converts the PDF pages to individual PNGs which are
    compared with the ImageMagick "compare" utility. This slows down
    running these tests but makes them reliable across Cairo versions.
    
    Signed-off-by: Zoltán Böszörményi <zboszor@gmail.com>

---
+++ tests/execute_pdf_test.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+TEST=$1
+
+abs_srcdir=${abs_srcdir:-.}
+abs_builddir=${abs_builddir:-.}
+
+if [[ ! -x ${TEST} ]]; then
+	echo -e "[ \\033[1;31mERROR\\033[0;39m ] ${TEST} - does not exist"
+	exit 0
+fi
+
+OCRPT_TEST=1
+export OCRPT_TEST
+
+mkdir -p ${abs_builddir}/results
+./${TEST} 2>results/${TEST}.stderr >results/${TEST}.stdout
+
+if [[ -f ${abs_srcdir}/expected/${TEST}.stdout ]]; then
+	rm -f ${abs_srcdir}/expected/${TEST}.stdout.*.png
+	rm -f results/${TEST}.stdout.*.png
+
+	OUTEXP=$(ghostscript -dNOPAUSE -dBATCH -sDEVICE=png48 -r300 -sOutputFile=${abs_srcdir}/expected/${TEST}.stdout.%d.png ${abs_srcdir}/expected/${TEST}.stdout)
+	OUTRES=$(ghostscript -dNOPAUSE -dBATCH -sDEVICE=png48 -r300 -sOutputFile=results/${TEST}.stdout.%d.png results/${TEST}.stdout)
+	PAGESEXP=$(echo "$OUTEXP" | grep 'Processing pages .* through')
+	PAGESEXPEND=$(echo "$PAGESEXP" | sed 's/^Processing pages .* through \([^\.]*\).*$/\1/')
+	PAGESRES=$(echo "$OUTRES" | grep 'Processing pages .* through')
+	PAGESRESEND=$(echo "$PAGESRES" | sed 's/^Processing pages .* through \([^\.]*\).*$/\1/')
+
+	if [[ $PAGESEXPEND -ne $PAGESRESEND ]]; then
+		OUTDIFF="Number of expected pages ($PAGESEXPEND) differ from the number of result pages ($PAGESRESEND)"
+	else
+		for i in $(seq 1 $PAGESEXPEND) ; do
+			DIFF=$(compare -metric AE -fuzz 1% ${abs_srcdir}/expected/layout_pdf_xml_test.stdout.1.png results/layout_pdf_xml_test.stdout.1.png results/layout_pdf_xml_test.stdout.1.diff.png 2>&1)
+			if [[ $DIFF -gt 0 ]]; then
+				OUTDIFF="Page $i differs"
+				break
+			fi
+		done
+	fi
+else
+	OUTDIFF="expected/${TEST}.stdout does not exist"
+fi
+
+ERRDIFF=$(diff -durpN ${abs_srcdir}/expected/${TEST}.stderr ${abs_builddir}/results/${TEST}.stderr 2>/dev/null)
+ERRRET=$?
+
+if [[ -n "$OUTDIFF" ]]; then
+	ERROR=1
+fi
+if [[ $ERRRET -ne 0 ]]; then
+	ERRERROR=1
+fi
+if [[ -n "$ERRDIFF" ]]; then
+	ERRERROR=1
+fi
+if [[ $ERROR -eq 1 ]]; then
+	echo -e "[ \\033[1;31mERROR\\033[0;39m ] ${TEST}"
+	echo "---- OUTDIFF ----"
+	echo "${OUTDIFF}"
+elif [[ $ERRERROR -eq 1 ]]; then
+	echo -e "[ \\033[1;33mWARNING\\033[0;39m ] ${TEST}"
+	echo "---- ERRDIFF ----"
+	echo "${ERRDIFF}"
+else
+	echo -e "[ \\033[38;05;34mOK\\033[0;39m ] ${TEST}"
+fi
