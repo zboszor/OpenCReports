9987a3e489681334b117ea92783029a7601d6b79
Zoltán Böszörményi <zboszor@gmail.com>
2022-11-07 05:56:15 +0100

    Tweak test executor scripts
    Export environment variables related to source and build
    directories, so individual runs of tests can produce the
    same results as when run from "make test" or "make pdf-test".
    
    Signed-off-by: Zoltán Böszörményi <zboszor@gmail.com>

--- tests/execute_pdf_test.sh
+++ tests/execute_pdf_test.sh
@@ -5,6 +5,8 @@ TEST=$1
 abs_srcdir=${abs_srcdir:-$(pwd)}
 abs_builddir=${abs_builddir:-$(pwd)}
 
+export abs_srcdir abs_builddir
+
 if [[ ! -x ${TEST} ]]; then
 	echo -e "[ \\033[1;31mERROR\\033[0;39m ] ${TEST} - does not exist"
 	exit 0
