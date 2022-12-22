346fb1c99b34ddc28e9e51b49555c641d6e065b0
Zoltán Böszörményi <zboszor@gmail.com>
2022-12-22 06:43:01 +0100

    Extend tests for PHP
    New target: "make php-pdf-test"
    
    The new executor script creates differently named stderr and
    PDF outputs, so the results can be distinguished from the C
    unit tests.
    
    Added a new PHP unit test called layout_pdf_xml37_test.php
    which is a rewrite of the previously existing C unit test called
    layout_pdf_xml37_test.c. The expected PDF output of the new PHP
    test was copied from the C test as a proof that the output is
    identical, regardless of the unit test language.
    
    Signed-off-by: Zoltán Böszörményi <zboszor@gmail.com>

---
+++ tests/layout_pdf_xml37_test.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * OpenCReports test
+ * Copyright (C) 2019-2022 Zoltán Böszörményi <zboszor@gmail.com>
+ * See COPYING.PHP in the toplevel directory.
+ */
+
+/*
+ * THIS NEEDS THAT THE PHP MODULE WILL DISCOVER ARRAYS FROM GLOBALS
+ */
+$array = [
+	[ "id", "name", "property", "age", "adult" ]
+];
+
+$coltypes = [
+	OpenCReport::RESULT_NUMBER, OpenCReport::RESULT_STRING, OpenCReport::RESULT_STRING, OpenCReport::RESULT_NUMBER, OpenCReport::RESULT_NUMBER
+];
+
+$srcdir = getenv("abs_srcdir");
+if (is_bool($srcdir))
+	$srcdir = getcwd();
+
+$srcdir = OpenCReport::canonicalize_path($srcdir);
+
+$o = new OpenCReport();
+
+if (!$o->parse_xml("layout_pdf_xml37_test.xml")) {
+	echo "XML parse error" . PHP_EOL;
+	exit(0);
+}
+
+$o->add_search_path($srcdir . "/images/images");
+
+$o->set_output_format(OpenCReport::OUTPUT_PDF);
+
+$o->execute();
+$o->spool();
