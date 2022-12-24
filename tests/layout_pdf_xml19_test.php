2277fda5a49f40984a953875cf0edc1b1a3213fc
Zoltán Böszörményi <zboszor@gmail.com>
2022-12-24 22:40:30 +0100

    Add many PHP unit tests
    Added layout_pdf_xml[3...42]_test.php as variants of the same
    numbered C unit tests. Their expected results are identical
    copies of the C unit tests' outputs.
    
    These were simple, they mostly rely on parsing XML report
    descriptions. As such, these unit tests work with the currently
    implemented PHP APIs.
    
    Signed-off-by: Zoltán Böszörményi <zboszor@gmail.com>

---
+++ tests/layout_pdf_xml19_test.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ * OpenCReports test
+ * Copyright (C) 2019-2022 Zoltán Böszörményi <zboszor@gmail.com>
+ * See COPYING.LGPLv3 in the toplevel directory.
+ */
+
+$array = [
+	[ "id", "name", "property", "age", "adult" ],
+	[ "1", "Fred Flintstone", "strong", "31", "yes" ],
+	[ "2", "Wilma Flintstone", "charming", "28", "yes" ],
+	[ "3", "Pebbles Flintstone", "young", "5e-1", "no" ],
+	[ "4", "Betty Rubble", "beautiful", "27", "yes" ],
+	[ "5", "Barney Rubble", "small", "29", "yes" ]
+];
+
+$coltypes = [
+	OpenCReport::RESULT_NUMBER, OpenCReport::RESULT_STRING, OpenCReport::RESULT_STRING, OpenCReport::RESULT_NUMBER, OpenCReport::RESULT_NUMBER
+];
+
+$array2 = [
+	[ "id", "name", "property", "age", "adult" ],
+	[ "1", "Mr. George Slate", "grumpy", "53", "yes" ],
+	[ "2", "Joe Rockhead", "friendly", "33", "yes" ],
+	[ "3", "Sam Slagheap", "leader", "37", "yes" ],
+	[ "4", "The Great Gazoo", "hostile alien", "1200", "yes" ],
+];
+
+$o = new OpenCReport();
+if (!$o->parse_xml("layout_pdf_xml19_test.xml")) {
+	echo "XML parse error" . PHP_EOL;
+	exit(0);
+}
+
+$o->set_output_format(OpenCReport::OUTPUT_PDF);
+
+$o->execute();
+$o->spool();
