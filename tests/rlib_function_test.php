fedc9780c10ba2447c0b2f8e44a16b380b7b77a3
Zoltán Böszörményi <zboszor@gmail.com>
2023-04-17 08:48:02 +0200

    Add a new PHP unit test
    Added a new PHP unit test called rlib_function_test to exercise
    custom report function via the RLIB compatibility API.
    
    Also fixed PHP unit test targets so they don't depend on the
    C unit tests to get built.
    
    Signed-off-by: Zoltán Böszörményi <zboszor@gmail.com>

---
+++ tests/rlib_function_test.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * OpenCReports test
+ * Copyright (C) 2019-2022 Zoltán Böszörményi <zboszor@gmail.com>
+ * See COPYING.LGPLv3 in the toplevel directory.
+ */
+
+require_once 'test_common.php';
+
+function my_add_func($a, $b) {
+	return $a + $b;
+}
+
+function my_concat_func($s, $n) {
+	return str_repeat($s, $n);
+}
+
+$o = rlib_init();
+rlib_add_function($o, "my_add", "my_add_func", 2);
+rlib_add_function($o, "my_concat", "my_concat_func", 2);
+
+$e = $o->expr_parse("my_add(1,2)");
+$e->optimize();
+eval_print_expr($e);
+
+$e = $o->expr_parse("my_concat('aaa', 3, 4)");
+echo $o->expr_error() . PHP_EOL;
+
+$e = $o->expr_parse("my_concat('abc', 3)");
+$e->optimize();
+eval_print_expr($e);
