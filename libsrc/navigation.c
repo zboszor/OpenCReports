61eaa8feec065335d5d7d952fca3711b553db3ca
Zoltán Böszörményi <zboszor@gmail.com>
2023-05-25 10:37:05 +0200

    Fix header comment in functions.c and navigation.c
    Signed-off-by: Zoltán Böszörményi <zboszor@gmail.com>

--- libsrc/navigation.c
+++ libsrc/navigation.c
@@ -1,5 +1,5 @@
 /*
- * OpenCReports main module
+ * OpenCReports query navigation handling
  * Copyright (C) 2019-2023 Zoltán Böszörményi <zboszor@gmail.com>
  * See COPYING.LGPLv3 in the toplevel directory.
  */
