4a55fa380315991e9d00ab9bb95e5d8307cfc832
Zoltán Böszörményi <zboszor@gmail.com>
2023-05-25 13:23:02 +0200

    Add XML skeleton driver code
    Signed-off-by: Zoltán Böszörményi <zboszor@gmail.com>

--- libsrc/xml-output.c
+++ libsrc/xml-output.c
@@ -1,5 +1,5 @@
 /*
- * OpenCReports main module
+ * OpenCReports XML output driver
  * Copyright (C) 2019-2023 Zoltán Böszörményi <zboszor@gmail.com>
  * See COPYING.LGPLv3 in the toplevel directory.
  */
@@ -9,8 +9,101 @@
 #include <string.h>
 
 #include "ocrpt-private.h"
+#include "parts.h"
 #include "xml-output.h"
+
+static void ocrpt_xml_add_new_page(opencreport *o, ocrpt_part *p, ocrpt_part_row *pr, ocrpt_part_column *pd, ocrpt_report *r, unsigned int rows, bool *newpage, double *page_indent, double *page_position, double *old_page_position) {
+	xml_private_data *priv = o->output_private;
+
+	if (o->precalculate) {
+		if (!priv->current_page) {
+			if (!priv->pages)
+				priv->pages = ocrpt_list_end_append(priv->pages, &priv->last_page, NULL);
+			priv->current_page = priv->pages;
+		} else {
+			mpfr_add_ui(o->pageno->number, o->pageno->number, 1, o->rndmode);
+			priv->pages = ocrpt_list_end_append(priv->pages, &priv->last_page, NULL);
+			priv->current_page = priv->last_page;
+		}
+
+		if (mpfr_cmp(o->totpages->number, o->pageno->number) < 0)
+			mpfr_set(o->totpages->number, o->pageno->number, o->rndmode);
+	} else {
+		if (!priv->current_page) {
+			priv->current_page = priv->pages;
+		} else {
+			mpfr_add_ui(o->pageno->number, o->pageno->number, 1, o->rndmode);
+			priv->current_page = priv->current_page->next;
+		}
+	}
+}
+
+static void *ocrpt_xml_get_current_page(opencreport *o) {
+	xml_private_data *priv = o->output_private;
+
+	return priv->current_page;
+}
+
+static void ocrpt_xml_set_current_page(opencreport *o, void *page) {
+	xml_private_data *priv = o->output_private;
+
+	priv->current_page = page;
+}
+
+static bool ocrpt_xml_is_current_page_first(opencreport *o) {
+	xml_private_data *priv = o->output_private;
+
+	return priv->current_page == priv->pages;
+}
+
+static void ocrpt_xml_start_data_row(opencreport *o, ocrpt_part *p, ocrpt_part_row *pr, ocrpt_part_column *pd, ocrpt_report *r, ocrpt_output *output, ocrpt_line *l, double page_indent, double y) {
+}
+
+static void ocrpt_xml_end_data_row(opencreport *o, ocrpt_part *p, ocrpt_part_row *pr, ocrpt_part_column *pd, ocrpt_report *r, ocrpt_output *output, ocrpt_line *l) {
+}
+
+static void ocrpt_xml_draw_text(opencreport *o, ocrpt_part *p, ocrpt_part_row *pr, ocrpt_part_column *pd, ocrpt_report *r, ocrpt_output *output, ocrpt_line *l, ocrpt_text *le, double page_width, double page_indent, double y) {
+	if (le->value || le->result_str->len)
+		fprintf(stderr, "ocrpt_xml_draw_text: %s\n", le->result_str->str);
+}
+
+static void ocrpt_xml_draw_image(opencreport *o, ocrpt_part *p, ocrpt_part_row *pr, ocrpt_part_column *pd, ocrpt_report *r, ocrpt_output *output, ocrpt_line *line, ocrpt_image *img, double page_width, double page_indent, double x, double y, double w, double h) {
+	fprintf(stderr, "ocrpt_xml_draw_image: -\n");
+}
+
+static void ocrpt_xml_finalize(opencreport *o) {
+	xml_private_data *priv = o->output_private;
+
+	ocrpt_list_free(priv->pages);
+	ocrpt_mem_string_free(priv->data, true);
+	ocrpt_mem_free(priv);
+	o->output_private = NULL;
+
+	ocrpt_string **content_type = ocrpt_mem_malloc(4 * sizeof(ocrpt_string *));
+	content_type[0] = ocrpt_mem_string_new_printf("Content-Type: text/xml; charset=utf-8\n");
+	content_type[1] = ocrpt_mem_string_new_printf("Content-Length: %zu", o->output_buffer->len);
+	content_type[2] = ocrpt_mem_string_new_printf("Content-Disposition: attachment; filename=%s", o->csv_filename ? o->csv_filename : "report.xml");
+	content_type[3] = NULL;
+	o->content_type = (const ocrpt_string **)content_type;
+}
 
 void ocrpt_xml_init(opencreport *o) {
 	memset(&o->output_functions, 0, sizeof(ocrpt_output_functions));
+	o->output_functions.start_data_row = ocrpt_xml_start_data_row;
+	o->output_functions.end_data_row = ocrpt_xml_end_data_row;
+	o->output_functions.draw_image = ocrpt_xml_draw_image;
+	o->output_functions.draw_text = ocrpt_xml_draw_text;
+	o->output_functions.add_new_page = ocrpt_xml_add_new_page;
+	o->output_functions.get_current_page = ocrpt_xml_get_current_page;
+	o->output_functions.set_current_page = ocrpt_xml_set_current_page;
+	o->output_functions.is_current_page_first = ocrpt_xml_is_current_page_first;
+	o->output_functions.finalize = ocrpt_xml_finalize;
+
+	o->output_private = ocrpt_mem_malloc(sizeof(xml_private_data));
+	memset(o->output_private, 0, sizeof(xml_private_data));
+
+	xml_private_data *priv = o->output_private;
+	priv->data = ocrpt_mem_string_new_with_len(NULL, 4096);
+
+	o->output_buffer = ocrpt_mem_string_new_with_len(NULL, 65536);
 }
--- libsrc/xml-output.h
+++ libsrc/xml-output.h
@@ -1,5 +1,5 @@
 /*
- * OpenCReports main module
+ * OpenCReports XML output driver
  * Copyright (C) 2019-2023 Zoltán Böszörményi <zboszor@gmail.com>
  * See COPYING.LGPLv3 in the toplevel directory.
  */
@@ -7,6 +7,14 @@
 #define _OCRPT_XML_H_
 
 #include "opencreport.h"
+
+struct xml_private_data {
+	ocrpt_string *data;
+	ocrpt_list *pages;
+	ocrpt_list *last_page;
+	ocrpt_list *current_page;
+};
+typedef struct xml_private_data xml_private_data;
 
 void ocrpt_xml_init(opencreport *o);
 
