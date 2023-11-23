f5d81fed23e2d1159046731cf5e2b5c237adf8da
Zoltán Böszörményi <zboszor@gmail.com>
2023-11-23 12:26:25 +0100

    Fix RLIB compatible code example
    Signed-off-by: Zoltán Böszörményi <zboszor@gmail.com>

--- examples/example1/example1rlib.php
+++ examples/example1/example1rlib.php
@@ -1,8 +1,6 @@
 <?php
 $r = rlib_init();
 
-rlib_add_report($r, "layout_ocrpt_fixed_part_test.xml");
-
 if (!rlib_add_report($r, "example1.xml")) {
     echo "XML parse error" . PHP_EOL;
     exit(0);
