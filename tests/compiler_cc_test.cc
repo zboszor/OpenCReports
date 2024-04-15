1e0d182ea4e6c27998013256b0046ef879f522cf
Zoltán Böszörményi <zboszor@gmail.com>
2024-04-15 14:56:41 +0200

    Add a new C++ unit test
    Added a new C++ unit test called compiler_cc_test.cc to prove
    that the public header can be compiled with a C++ compiler.
    
    Signed-off-by: Zoltán Böszörményi <zboszor@gmail.com>

---
+++ tests/compiler_cc_test.cc
@@ -0,0 +1,11 @@
+/*
+ * OpenCReports test
+ * Copyright (C) 2019-2024 Zoltán Böszörményi <zboszor@gmail.com>
+ * See COPYING.LGPLv3 in the toplevel directory.
+ */
+
+#include <opencreport.h>
+
+int main(int argc, char *argv[]) {
+	return 0;
+}
