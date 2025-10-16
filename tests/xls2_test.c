b269b9ce572923bbc828ee6b055d85d649f00180
Zoltán Böszörményi <zboszor@gmail.com>
2025-10-16 17:49:01 +0200

    Exercise spreadsheet datasource from report description
    Added a new C unit test called xls2_test.c to exercise the
    spreadsheet datasource from the report description using
    <Datasource> and <Query> nodes.
    
    Signed-off-by: Zoltán Böszörményi <zboszor@gmail.com>

---
+++ tests/xls2_test.c
@@ -0,0 +1,73 @@
+/*
+ * OpenCReports test
+ * Copyright (C) 2019-2025 Zoltán Böszörményi <zboszor@gmail.com>
+ * See COPYING.LGPLv3 in the toplevel directory.
+ */
+
+#include <stdio.h>
+
+#include <opencreport.h>
+#include "test_common.h"
+
+struct rowdata {
+	ocrpt_query *q;
+	int32_t row;
+};
+
+static void test_newrow_cb(opencreport *o, ocrpt_report *r, void *ptr) {
+	struct rowdata *rd = ptr;
+	ocrpt_query_result *qr;
+	int32_t cols;
+
+	qr = ocrpt_query_get_result(rd->q, &cols);
+
+	printf("Row #%d\n", rd->row++);
+	print_result_row("a", qr, cols);
+
+	printf("\n");
+}
+
+int main(int argc, char **argv) {
+	opencreport *o;
+	ocrpt_datasource *ds;
+	ocrpt_report *r;
+	struct rowdata rd = { .row = 0 };
+
+	o = ocrpt_init();
+
+	if (!ocrpt_parse_xml(o, "xlsds.xml")) {
+		printf("XML parse error\n");
+		ocrpt_free(o);
+		return 0;
+	}
+
+	ds = ocrpt_datasource_get(o, "pandas");
+	if (!ds) {
+		fprintf(stderr, "Failed to add a pandas datasource.\n");
+		ocrpt_free(o);
+		return 1;
+	}
+
+	r = ocrpt_part_column_new_report(ocrpt_part_row_new_column(ocrpt_part_new_row(ocrpt_part_new(o))));
+
+	rd.q = ocrpt_query_get(o, "a");
+	if (!rd.q) {
+		fprintf(stderr, "Failed to add a spreadsheet query.\n");
+		ocrpt_free(o);
+		return 1;
+	}
+
+	ocrpt_report_set_main_query(r, rd.q);
+
+	if (!ocrpt_report_add_new_row_cb(r, test_newrow_cb, &rd)) {
+		fprintf(stderr, "Failed to add new row callback.\n");
+		ocrpt_free(o);
+		return  1;
+	}
+
+	ocrpt_execute(o);
+
+	ocrpt_free(o);
+
+	return 0;
+}
