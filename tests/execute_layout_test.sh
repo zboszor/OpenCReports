77e4f507997c14395ba7cd6353e012327ea0d34e
Zoltán Böszörményi <zboszor@gmail.com>
2025-10-18 11:54:34 +0200

    Allow layout unit test result variants
    Due to differences between font versions, Pango and other
    packages, the layout rendering may differ. This does not make
    the unit test result invalid, just different.
    
    In the past, when my build machine was upgraded, such differences
    were shrugged off and the test results were updated with the more
    recently correct ones. Recently, some package update has "broken"
    a few unit test result in the same way as before.
    
    But enough is enough. Keep old test results, and allow new
    variants of layout unit test results. The unit test scripts
    were updated for this.
    
    Added new variants of the results of the newly broken tests.
    
    In passing, merge the test script differences between
    execute_layout_test.sh and execute_php_layout_test.sh, and
    between execute_test.sh and execute_php_test.sh. As a result,
    deleted execute_php_layout_test.sh and execute_php_test.sh
    and use execute_layout_test.sh and execute_test.sh from this point
    for both C and PHP unit tests. With only small changes, this
    should allow unit tests with language bindings when they get
    implemented.
    
    Signed-off-by: Zoltán Böszörményi <zboszor@gmail.com>

--- tests/execute_layout_test.sh
+++ tests/execute_layout_test.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 function compare_pdf () {
-	OUTEXP=$(ghostscript -dNOPAUSE -dBATCH -sDEVICE=png48 -r150 -sOutputFile=results/"${TEST}"."${SFX[$TYPE]}".exp.%d.png "${abs_srcdir}"/expected/"${TEST}"."${SFX[$TYPE]}")
-	OUTRES=$(ghostscript -dNOPAUSE -dBATCH -sDEVICE=png48 -r150 -sOutputFile=results/"${TEST}"."${SFX[$TYPE]}".%d.png results/"${TEST}"."${SFX[$TYPE]}")
+	local EXPECTED_PDF
+	EXPECTED_PDF=$(basename "$1")
+	OUTDIFF=
+	WARNINGS=
+	OUTEXP=$(ghostscript -dNOPAUSE -dBATCH -sDEVICE=png48 -r150 -sOutputFile="${abs_builddir}/results/${EXPECTED_PDF}.exp.%d.png" "$1")
+	OUTRES=$(ghostscript -dNOPAUSE -dBATCH -sDEVICE=png48 -r150 -sOutputFile="${abs_builddir}/results/${TEST}${TESTSFX}.${SFX[$TYPE]}.%d.png" "${abs_builddir}/results/${TEST}${TESTSFX}.${SFX[$TYPE]}")
 	PAGESEXP=$(echo "$OUTEXP" | grep 'Processing pages .* through')
 	PAGESEXPEND=$(echo "$PAGESEXP" | sed 's/^Processing pages .* through \([^\.]*\).*$/\1/')
 	PAGESRES=$(echo "$OUTRES" | grep 'Processing pages .* through')
@@ -12,7 +16,7 @@ function compare_pdf () {
 		OUTDIFF="Number of expected pages ($PAGESEXPEND) differ from the number of result pages ($PAGESRESEND)"
 	else
 		for i in $(seq 1 "$PAGESEXPEND") ; do
-			DIFF=$(compare -metric AE -fuzz '1%' results/"${TEST}"."${SFX[$TYPE]}".exp."${i}".png results/"${TEST}"."${SFX[$TYPE]}"."${i}".png -colorspace RGB results/"${TEST}"."${SFX[$TYPE]}"."${i}".diff.png 2>&1)
+			DIFF=$(compare -metric AE -fuzz '1%' "${abs_builddir}/results/${EXPECTED_PDF}.exp.${i}.png" "${abs_builddir}/results/${TEST}${TESTSFX}.${SFX[$TYPE]}.${i}.png" -colorspace RGB "${abs_builddir}/results/${TEST}${TESTSFX}.${SFX[$TYPE]}.${i}.diff.png" 2>&1)
 			DIFF1=$(echo "$DIFF" | sed 's#^\([0-9]*\)compare:.*$#\1#')
 			DIFF2=$(echo "$DIFF" | sed 's#^\([0-9]*\) .*$#\1#')
 			if [[ $DIFF1 = "0" ]] || [[ $DIFF2 = "0" ]] ; then
@@ -44,8 +48,8 @@ top_srcdir=${top_srcdir:-$(dirname $(readlink -f "$0"))/..}
 
 export abs_srcdir abs_builddir
 
-if [[ ! -x ${TEST} ]]; then
-	echo -e "[ \\033[1;31mERROR\\033[0;39m ] ${TEST} - does not exist"
+if [[ ! -f ${TEST}${TESTSFX} ]]; then
+	echo -e "[ \\033[1;31mERROR\\033[0;39m ] ${TEST}${TESTSFX} - does not exist"
 	exit 0
 fi
 
@@ -55,53 +59,50 @@ export OCRPT_TEST
 SFX=("unknown" "pdf" "html" "txt" "csv" "xml" "json")
 
 mkdir -p "${abs_builddir}"/results
-case "$TYPE" in
-1)
-	rm -f results/"${TEST}"."${SFX[$TYPE]}".*.png results/"${TEST}"."${SFX[$TYPE]}".asanout.*
-	;;
-*)
-	rm -f results/"${TEST}"."${SFX[$TYPE]}".asanout.*
-	;;
-esac
+rm -f "${abs_builddir}/results/${TEST}${TESTSFX}.${SFX[$TYPE]}".*.png
+rm -f "${abs_builddir}/results/${TEST}${TESTSFX}.${SFX[$TYPE]}".asanout.*
 
-export ASAN_OPTIONS="log_path=results/${TEST}.${SFX[$TYPE]}.asanout,fast_unwind_on_malloc=0"
+export ASAN_OPTIONS="log_path=${abs_builddir}/results/${TEST}${TESTSFX}.${SFX[$TYPE]}.asanout,fast_unwind_on_malloc=0"
 export UBSAN_OPTIONS=print_stacktrace=true
 export LSAN_OPTIONS="suppressions=${top_srcdir}/ignore.supp"
-"./${TEST}" "${TYPE}" 2>"results/${TEST}.${SFX[$TYPE]}.stderr" >"results/${TEST}.${SFX[$TYPE]}"
 
-WARNINGS=
-case $TYPE in
-1)
-	if [[ -f ${abs_srcdir}/expected/${TEST}.${SFX[$TYPE]} ]]; then
-		compare_pdf
+if [[ $TESTSFX == '.php' ]]; then
+	php "${TEST}${TESTSFX}" "${TYPE}" 2>"${abs_builddir}/results/${TEST}${TESTSFX}.${SFX[$TYPE]}.stderr" >"${abs_builddir}/results/${TEST}${TESTSFX}.${SFX[$TYPE]}"
+else
+	"./${TEST}${TESTSFX}" "${TYPE}" 2>"${abs_builddir}/results/${TEST}${TESTSFX}.${SFX[$TYPE]}.stderr" >"${abs_builddir}/results/${TEST}${TESTSFX}.${SFX[$TYPE]}"
+fi
+
+if [[ -f "${abs_srcdir}/expected/${TEST}${TESTSFX}.${SFX[$TYPE]}" ]]; then
+	EXPECTED_TEST="${abs_srcdir}/expected/${TEST}${TESTSFX}.${SFX[$TYPE]}"
+	if [[ $TYPE = 1 ]]; then
+		compare_pdf "$EXPECTED_TEST"
 	else
-		OUTDIFF="expected/${TEST}.${SFX[$TYPE]} does not exist"
+		OUTDIFF=$(diff -durpN "$EXPECTED_TEST" "${abs_builddir}/results/${TEST}${TESTSFX}.${SFX[$TYPE]}" 2>/dev/null)
 	fi
-	;;
-*)
-	if [[ -f ${abs_srcdir}/expected/${TEST}.${SFX[$TYPE]} ]]; then
-		OUTDIFF=$(diff -durpN ${abs_srcdir}/expected/${TEST}.${SFX[$TYPE]} ${abs_srcdir}/results/${TEST}.${SFX[$TYPE]} 2>/dev/null)
-	else
-		FOUND=
-		for i in ${abs_srcdir}/expected/${TEST}.${SFX[$TYPE]}* ; do
-			if [[ -f "$i" ]]; then
-				OUTDIFF=$(diff -durpN "$i" ${abs_srcdir}/results/${TEST}.${SFX[$TYPE]} 2>/dev/null)
-				if [[ -z "$OUTDIFF" ]]; then
-					FOUND=1
-					break
-				fi
+else
+	FOUND=
+	for EXPECTED_TEST in "${abs_srcdir}/expected/${TEST}${TESTSFX}.${SFX[$TYPE]}"* ; do
+		if [[ -f "$EXPECTED_TEST" ]]; then
+			[[ "$EXPECTED_TEST" =~ \.stderr$ ]] && continue
+			if [[ $TYPE = 1 ]]; then
+				compare_pdf "$EXPECTED_TEST"
 			else
-				OUTDIFF="${abs_srcdir}/expected/${TEST}.${SFX[$TYPE]} or a variant does not exist."
+				OUTDIFF=$(diff -durpN "$EXPECTED_TEST" "${abs_builddir}/results/${TEST}${TESTSFX}.${SFX[$TYPE]}" 2>/dev/null)
 			fi
-		done
-		if [[ -z "$FOUND" ]]; then
-			OUTDIFF="No matching expected result found"
+			if [[ -z $OUTDIFF ]]; then
+				FOUND=1
+				break
+			fi
+		else
+			OUTDIFF="${abs_srcdir}/expected/${TEST}${TESTSFX}.${SFX[$TYPE]} or a variant does not exist"
 		fi
+	done
+	if [[ -z "$FOUND" ]]; then
+		OUTDIFF="No matching expected result found"
 	fi
-	;;
-esac
+fi
 
-ERRDIFF=$(diff -durpN "${abs_srcdir}/expected/${TEST}.${SFX[$TYPE]}.stderr" "${abs_builddir}/results/${TEST}.${SFX[$TYPE]}.stderr" 2>/dev/null)
+ERRDIFF=$(diff -durpN "${EXPECTED_TEST}.stderr" "${abs_builddir}/results/${TEST}${TESTSFX}.${SFX[$TYPE]}.stderr" 2>/dev/null)
 ERRRET=$?
 
 if [[ -n "$OUTDIFF" ]]; then
--- tests/execute_test.sh
+++ tests/execute_test.sh
@@ -8,8 +8,8 @@ top_srcdir=${top_srcdir:-$(dirname $(readlink -f "$0"))/..}
 
 export abs_srcdir abs_builddir
 
-if [[ ! -x ${TEST} ]]; then
-	echo -e "[ \\033[1;31mERROR\\033[0;39m ] ${TEST} - does not exist"
+if [[ ! -f ${TEST}${TESTSFX} ]]; then
+	echo -e "[ \\033[1;31mERROR\\033[0;39m ] ${TEST}${TESTSFX} - does not exist"
 	exit 0
 fi
 
@@ -17,32 +17,36 @@ OCRPT_TEST=1
 export OCRPT_TEST
 
 mkdir -p ${abs_builddir}/results
-rm -f results/${TEST}.stdout.*.png results/${TEST}.asanout.*
-export ASAN_OPTIONS="log_path=results/${TEST}.asanout,fast_unwind_on_malloc=0"
+rm -f ${abs_builddir}/results/${TEST}${TESTSFX}.stdout.*.png ${abs_builddir}/results/${TEST}${TESTSFX}.asanout.*
+export ASAN_OPTIONS="log_path=${abs_builddir}/results/${TEST}${TESTSFX}.asanout,fast_unwind_on_malloc=0"
 export UBSAN_OPTIONS=print_stacktrace=true
 export LSAN_OPTIONS="suppressions=${top_srcdir}/ignore.supp"
-./${TEST} 2>results/${TEST}.stderr >results/${TEST}.stdout
+if [[ $TESTSFX == '.php' ]]; then
+	php ${TEST}${TESTSFX} 2>${abs_builddir}/results/${TEST}${TESTSFX}.stderr >${abs_builddir}/results/${TEST}${TESTSFX}.stdout
+else
+	./${TEST}${TESTSFX} 2>${abs_builddir}/results/${TEST}${TESTSFX}.stderr >${abs_builddir}/results/${TEST}${TESTSFX}.stdout
+fi
 
-if [[ -f ${abs_srcdir}/expected/${TEST}.stdout ]]; then
-	OUTDIFF=$(diff -durpN ${abs_srcdir}/expected/${TEST}.stdout ${abs_srcdir}/results/${TEST}.stdout 2>/dev/null)
+if [[ -f ${abs_srcdir}/expected/${TEST}${TESTSFX}.stdout ]]; then
+	OUTDIFF=$(diff -durpN ${abs_srcdir}/expected/${TEST}${TESTSFX}.stdout ${abs_builddir}/results/${TEST}${TESTSFX}.stdout 2>/dev/null)
 	OUTRET=$?
-	ERRDIFF=$(diff -durpN ${abs_srcdir}/expected/${TEST}.stderr ${abs_srcdir}/results/${TEST}.stderr 2>/dev/null)
+	ERRDIFF=$(diff -durpN ${abs_srcdir}/expected/${TEST}${TESTSFX}.stderr ${abs_builddir}/results/${TEST}${TESTSFX}.stderr 2>/dev/null)
 	ERRRET=$?
 else
 	FOUND=
-	for i in ${abs_srcdir}/expected/${TEST}.stdout* ; do
+	for i in ${abs_srcdir}/expected/${TEST}${TESTSFX}.stdout* ; do
 		if [[ -f "$i" ]]; then
-			SFX=${i#${abs_srcdir}/expected/${TEST}.stdout}
-			OUTDIFF=$(diff -durpN "$i" ${abs_srcdir}/results/${TEST}.stdout 2>/dev/null)
+			SFX=${i#${abs_srcdir}/expected/${TEST}${TESTSFX}.stdout}
+			OUTDIFF=$(diff -durpN "$i" ${abs_builddir}/results/${TEST}${TESTSFX}.stdout 2>/dev/null)
 			OUTRET=$?
-			ERRDIFF=$(diff -durpN ${abs_srcdir}/expected/${TEST}.stderr${SFX} ${abs_srcdir}/results/${TEST}.stderr 2>/dev/null)
+			ERRDIFF=$(diff -durpN ${abs_srcdir}/expected/${TEST}${TESTSFX}.stderr${SFX} ${abs_builddir}/results/${TEST}${TESTSFX}.stderr 2>/dev/null)
 			ERRRET=$?
 			if [[ -z "$OUTDIFF" ]]; then
 				FOUND=1
 				break
 			fi
 		else
-			OUTDIFF="${abs_srcdir}/expected/${TEST}.stdout or a variant does not exist."
+			OUTDIFF="${abs_srcdir}/expected/${TEST}${TESTSFX}.stdout or a variant does not exist."
 		fi
 	done
 fi
